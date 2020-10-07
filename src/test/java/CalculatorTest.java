23ecdef289cf95fb4ec424914e42d21f6f43cff3
Martin <martin.blomberg@outlook.com>
2020-10-07 10:40:05 +0200

    Added first test and updated java version

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("Add method, two values as input returns sum of values.")
+    void addWithTwoValuesReturnsSumOfValues() {
+        assertTrue(false);
+    }
+
+
+}
