0da264508018b48b7602e41108f195ce0dbf462c
Martin Blomberg <martin.blomberg@outlook.com>
2020-10-14 12:23:26 +0200

    Custom delimiters

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,6 +6,15 @@ public class StringCalculator {
     public int add(String numbers) {
         if(nullOrEmpty(numbers))
             return 0;
+        if( numbers.startsWith("//"))
+        {
+            var customDelimiter = numbers.charAt(2);
+            return Arrays.stream(numbers.substring(numbers.indexOf('\n')+1)
+                    .split(String.valueOf(customDelimiter)))
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+        }
+
         return Arrays.stream(numbers.split("[,\n]")).mapToInt(Integer::parseInt).sum();
     }
 
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -9,35 +9,39 @@ public class StringCalculatorTest {
     StringCalculator stringCalculator = new StringCalculator();
 
     @Test
-    public void callToAddWithEmptyStringReturnsZero()
-    {
+    public void callToAddWithEmptyStringReturnsZero() {
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    public void stringWithOneNumberReturnsThatNumber(){
-        assertEquals(1,stringCalculator.add("1"));
+    public void stringWithOneNumberReturnsThatNumber() {
+        assertEquals(1, stringCalculator.add("1"));
     }
 
     @Test
-    public void nullReferenceReturnsZeroWithoutCrashing(){
+    public void nullReferenceReturnsZeroWithoutCrashing() {
         assertEquals(0, stringCalculator.add(null));
     }
 
     @Test
-    public void stringWithTwoNumbersWithCommaSeparatorReturnsSumOfNumbers(){
+    public void stringWithTwoNumbersWithCommaDelimiterReturnsSumOfNumbers() {
         assertEquals(3, stringCalculator.add("1,2"));
     }
 
     @Test
-    public void stringWithManyNumbersWithCommaSeparatorReturnsSumOfNumbers(){
+    public void stringWithManyNumbersWithCommaDelimiterReturnsSumOfNumbers() {
         assertEquals(15, stringCalculator.add("1,2,3,4,5"));
     }
+
     @Test
-    public void acceptingBothNewLineAndCommaAsSeparators(){
-        assertEquals(6,stringCalculator.add("1\n2,3"));
+    public void acceptingBothNewLineAndCommaAsDelimiter() {
+        assertEquals(6, stringCalculator.add("1\n2,3"));
     }
 
+    @Test
+    public void customDelimiter() {
+        assertEquals(6, stringCalculator.add("//;\n1;2;3"));
+    }
 
 
 }
