d470f4458d9fd40296ec22ad1e7ea77d355c5ac0
Martin Blomberg <martin.blomberg@outlook.com>
2020-10-14 12:25:55 +0200

    Refactored checks into own methods

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,16 +6,23 @@ public class StringCalculator {
     public int add(String numbers) {
         if(nullOrEmpty(numbers))
             return 0;
-        if( numbers.startsWith("//"))
+        if(usesCustomDelimiter(numbers))
         {
-            var customDelimiter = numbers.charAt(2);
+            char customDelimiter = getCustomDelimiter(numbers);
             return Arrays.stream(numbers.substring(numbers.indexOf('\n')+1)
                     .split(String.valueOf(customDelimiter)))
                     .mapToInt(Integer::parseInt)
                     .sum();
         }
-
         return Arrays.stream(numbers.split("[,\n]")).mapToInt(Integer::parseInt).sum();
+    }
+
+    private char getCustomDelimiter(String numbers) {
+        return numbers.charAt(2);
+    }
+
+    private boolean usesCustomDelimiter(String numbers) {
+        return numbers.startsWith("//");
     }
 
     private boolean nullOrEmpty(String numbers) {
