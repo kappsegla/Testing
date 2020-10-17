53122d9f85fbb1c2d67cd5182f118abdf4316fec
Martin Blomberg <martin.blomberg@outlook.com>
2020-10-17 21:07:06 +0200

    Added dependencies and example code to start writing Integration Tests

---
+++ src/test/java/calculator/ITCalculatorTest.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Disabled
+class ITCalculatorTest {
+
+
+    @Test
+    void integrationTestExample() {
+        throw new RuntimeException("This is an integration test.");
+    }
+
+
+}
